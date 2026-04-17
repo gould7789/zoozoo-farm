457af305849cabf44e5b46450bfa63f9c2f4a668
Lee Hyeonwoo <hyeon.woo7789@gmail.com>
2026-04-17 15:40:46 +0900

    db: add hidden boolean column to animal_categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_16_170457) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_17_050723) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
 
   create_table "animal_categories", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.boolean "hidden", default: false, null: false
     t.string "name", limit: 100, null: false
     t.datetime "updated_at", null: false
     t.uuid "zone_id", null: false

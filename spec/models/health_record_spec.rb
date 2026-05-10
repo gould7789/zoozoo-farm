f90112415a60dbcdd444dcc077409f2764fbe71d
Lee Hyeonwoo <hyeon.woo7789@gmail.com>
2026-05-10 09:53:22 +0900

    refactor: activate user association tests and unify is_expected.to style

--- spec/models/health_record_spec.rb
+++ spec/models/health_record_spec.rb
@@ -1,35 +1,26 @@
-# HealthRecordモデルのバリデーション・アソシエーション・enum・スコープをテストする
 require "rails_helper"
 
 RSpec.describe HealthRecord, type: :model do
   describe "バリデーション" do
-    # 観察日は必須（created_atと分離して実際の観察日を記録する）
-    it { should validate_presence_of(:recorded_on) }
+    it { is_expected.to validate_presence_of(:recorded_on) }
   end
 
   describe "アソシエーション" do
-    # 健康記録は必ずどこかの動物に紐づく
-    it { should belong_to(:animal) }
-    # 作成者はcreated_byカラムでusersを参照する
-    it { should belong_to(:created_by).class_name("User") }
+    it { is_expected.to belong_to(:animal) }
+    it { is_expected.to belong_to(:created_by).class_name("User") }
   end
 
   describe "Enum" do
-    # condition: 0=normal, 1=caution, 2=danger
-    it { should define_enum_for(:condition).with_values(normal: 0, caution: 1, danger: 2) }
+    it { is_expected.to define_enum_for(:condition).with_values(normal: 0, caution: 1, danger: 2) }
   end
 
   describe "デフォルト値" do
-    subject(:health_record) { HealthRecord.new }
-
-    # 特異事項がない限りは正常をデフォルトにする
     it "conditionのデフォルトはnormal" do
-      expect(health_record.condition).to eq("normal")
+      expect(HealthRecord.new.condition).to eq("normal")
     end
   end
 
   describe "スコープ" do
-    # 最新の観察日順に並べることで直近の状態を先頭に表示する
     it ".recentは観察日の降順で返す" do
       animal = create(:animal)
       user   = create(:user)
--- spec/models/notice_spec.rb
+++ spec/models/notice_spec.rb
@@ -1,17 +1,13 @@
-# Noticeモデルのバリデーション・アソシエーション・スコープをテストする
 require "rails_helper"
 
 RSpec.describe Notice, type: :model do
   describe "バリデーション" do
-    # 本文は必須
-    it { should validate_presence_of(:body) }
-    # カテゴリは必須
-    it { should validate_presence_of(:category) }
+    it { is_expected.to validate_presence_of(:body) }
+    it { is_expected.to validate_presence_of(:category) }
   end
 
   describe "アソシエーション" do
-    # 作成者はcreated_byカラムでusersを参照する
-    it { should belong_to(:created_by).class_name("User") }
+    it { is_expected.to belong_to(:created_by).class_name("User") }
   end
 
   describe "enum" do
@@ -29,7 +25,6 @@ RSpec.describe Notice, type: :model do
   end
 
   describe "スコープ" do
-    # 最新の投稿順に並べることで直近のお知らせを先頭に表示する
     it ".recentは作成日時の降順で返す" do
       user  = create(:user)
       older = create(:notice, created_by: user, created_at: 2.days.ago)
